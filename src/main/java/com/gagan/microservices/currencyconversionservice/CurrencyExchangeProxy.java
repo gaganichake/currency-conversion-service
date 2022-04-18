858f02135a728bd88e6dd8508d13688150b950dd
Gagan Ichake <gaganichake@yahoo.com>
2022-04-18 10:06:25 -0500

    Configure host name using ConfigMap

--- src/main/java/com/gagan/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/gagan/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange")
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000") // In the environment variable _SERVICE_HOST is a suffix will be added by Kubernetes
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_MY_URI:http://localhost}:8000") // CURRENCY_EXCHANGE_MY_URI is the custom environment variable
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000") // In the environment variable _SERVICE_HOST is a suffix will be added by Kubernetes
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_MY_URI:http://localhost}:8000") // CURRENCY_EXCHANGE_MY_URI is the custom environment variable
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
